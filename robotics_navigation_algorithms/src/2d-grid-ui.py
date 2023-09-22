578e310fc22c61b1cf65019efa7ad0aa26c328dc
Ashwin Bose <ashwinbose123@gmail.com>
2023-09-22 18:29:22 +0100

    add two buttons

--- robotics_navigation_algorithms/src/2d-grid-ui.py
+++ robotics_navigation_algorithms/src/2d-grid-ui.py
@@ -14,7 +14,7 @@ class PixelType(Enum):
 def GetPixelColourForType(type: PixelType):
     if type == PixelType.FREE_SPACE:
         return "white"
-    return "black"
+    return "red"
 
 
 class PixelElement:
@@ -37,6 +37,8 @@ class PixelElement:
 
 
 class Pixels:
+    button_type = PixelType.OBSTACLE
+
     def __init__(self, canvas) -> None:
         num_rows = canvas.winfo_reqheight() // PIXEL_SIZE
         num_cols = canvas.winfo_reqwidth() // PIXEL_SIZE
@@ -46,18 +48,31 @@ class Pixels:
             for i in range(num_rows)
         ]
 
-    def add_point(self, x, y, type: PixelType):
+    def add_point(self, x, y):
         row = y // PIXEL_SIZE
         col = x // PIXEL_SIZE
         canvas.itemconfig(
-            self.pixels[row][col].add_point([row, col], type),
-            fill=GetPixelColourForType(type),
+            self.pixels[row][col].add_point([row, col], self.button_type),
+            fill=GetPixelColourForType(self.button_type),
         )
+
+    def set_button_type(self, type: PixelType):
+        print(type)
+        self.button_type = type
+
+    def set_free_space(self):
+        self.set_button_type(PixelType.FREE_SPACE)
+
+    def set_obstacle(self):
+        self.set_button_type(PixelType.OBSTACLE)
 
 
 if __name__ == "__main__":
     window = tk.Tk()
     window.title("2D world")
+
+    frame = tk.Frame(window)
+    frame.pack()
 
     # Create a canvas widget for drawing
     canvas = tk.Canvas(
@@ -67,10 +82,24 @@ if __name__ == "__main__":
     pixels = Pixels(canvas)
 
     def on_canvas_click(event):
-        pixels.add_point(event.x, event.y, PixelType.OBSTACLE)
+        pixels.add_point(event.x, event.y)
 
     def on_canvas_drag(event):
-        pixels.add_point(event.x, event.y, PixelType.OBSTACLE)
+        pixels.add_point(event.x, event.y)
+
+    obstacle = tk.Button(
+        frame,
+        text="Obstacle",
+        fg="red",
+        command=pixels.set_obstacle,
+    )
+    obstacle.pack(side=tk.LEFT)
+
+    def write_slogan():
+        print("Tkinter is easy to use!")
+
+    free_space = tk.Button(frame, text="Free Space", command=pixels.set_free_space)
+    free_space.pack(side=tk.LEFT)
 
     canvas.bind("<Button-1>", on_canvas_click)
     canvas.bind("<B1-Motion>", on_canvas_drag)
