bbedfadbe9c5d650f4ee412e0ca7378137578504
Ashwin Bose <ashwinbose123@gmail.com>
2023-09-23 07:25:06 +0100

    Add reset button

--- robotics_navigation_algorithms/src/2d-grid-ui.py
+++ robotics_navigation_algorithms/src/2d-grid-ui.py
@@ -31,7 +31,7 @@ class PixelElement:
             fill=GetPixelColourForType(self.state),
         )
 
-    def add_point(self, index: list, type: PixelType):
+    def add_point(self, type: PixelType):
         self.state = type
         canvas.itemconfig(self.pixel, fill=GetPixelColourForType(type))
 
@@ -40,24 +40,26 @@ class Pixels:
     button_type = PixelType.OBSTACLE
 
     def __init__(self, canvas) -> None:
-        num_rows = canvas.winfo_reqheight() // PIXEL_SIZE
-        num_cols = canvas.winfo_reqwidth() // PIXEL_SIZE
+        self.num_rows = canvas.winfo_reqheight() // PIXEL_SIZE
+        self.num_cols = canvas.winfo_reqwidth() // PIXEL_SIZE
 
         self.pixels = [
-            [PixelElement(canvas, [i, j]) for j in range(num_cols)]
-            for i in range(num_rows)
+            [PixelElement(canvas, [i, j]) for j in range(self.num_cols)]
+            for i in range(self.num_rows)
         ]
 
-    def add_point(self, x, y):
-        row = y // PIXEL_SIZE
-        col = x // PIXEL_SIZE
+    def add_point(self, row, col):
         canvas.itemconfig(
-            self.pixels[row][col].add_point([row, col], self.button_type),
+            self.pixels[row][col].add_point(self.button_type),
             fill=GetPixelColourForType(self.button_type),
         )
 
+    def add_point_from_pixels(self, x, y):
+        row = y // PIXEL_SIZE
+        col = x // PIXEL_SIZE
+        self.add_point(row, col)
+
     def set_button_type(self, type: PixelType):
-        print(type)
         self.button_type = type
 
     def set_free_space(self):
@@ -66,42 +68,59 @@ class Pixels:
     def set_obstacle(self):
         self.set_button_type(PixelType.OBSTACLE)
 
+    def clear_all(self):
+        old_button_type = self.button_type
+        self.button_type = PixelType.FREE_SPACE
+        print(self.num_rows)
+        for i in range(self.num_rows):
+            for j in range(self.num_cols):
+                self.add_point(i, j)
+        self.button_type = old_button_type
+
 
 if __name__ == "__main__":
     window = tk.Tk()
     window.title("2D world")
 
-    frame = tk.Frame(window)
-    frame.pack()
-
-    # Create a canvas widget for drawing
+    # Canvas
     canvas = tk.Canvas(
         window, width=CANVAS_SIZE_PIXELS, height=CANVAS_SIZE_PIXELS, bg="white"
     )
-    canvas.pack()
     pixels = Pixels(canvas)
 
-    def on_canvas_click(event):
-        pixels.add_point(event.x, event.y)
+    # Frame
+    frame = tk.Frame(window)
 
-    def on_canvas_drag(event):
-        pixels.add_point(event.x, event.y)
-
+    # Buttons
     obstacle = tk.Button(
         frame,
         text="Obstacle",
         fg="red",
         command=pixels.set_obstacle,
     )
-    obstacle.pack(side=tk.LEFT)
 
     def write_slogan():
         print("Tkinter is easy to use!")
 
     free_space = tk.Button(frame, text="Free Space", command=pixels.set_free_space)
-    free_space.pack(side=tk.LEFT)
+
+    reset = tk.Button(frame, text="Reset", command=pixels.clear_all)
+
+    # Add callbacks
+    def on_canvas_click(event):
+        pixels.add_point_from_pixels(event.x, event.y)
+
+    def on_canvas_drag(event):
+        pixels.add_point_from_pixels(event.x, event.y)
 
     canvas.bind("<Button-1>", on_canvas_click)
     canvas.bind("<B1-Motion>", on_canvas_drag)
+
+    # Pack everything
+    frame.pack()
+    obstacle.pack(side=tk.LEFT)
+    free_space.pack(side=tk.LEFT)
+    reset.pack(side=tk.LEFT)
+    canvas.pack()
 
     window.mainloop()
